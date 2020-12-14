3e9cd1113e3d1eb2d274dc1cca33e6035d4113f9
Marcel <viehmaie@hs-pforzheim.de>
2020-12-14 21:27:01 +0100

    Added endpoint to return account from jwt

--- src/main/java/de/hspf/authservice/secure/ProtectedController.java
+++ src/main/java/de/hspf/authservice/secure/ProtectedController.java
@@ -1,5 +1,6 @@
 package de.hspf.authservice.secure;
 
+import de.hspf.authservice.Account;
 import java.util.Optional;
 import org.eclipse.microprofile.jwt.Claim;
 import org.eclipse.microprofile.jwt.ClaimValue;
@@ -9,22 +10,27 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 /**
  *
  */
-@Path("/protected")
+@Path("/user")
 @RequestScoped
 public class ProtectedController {
 
     @Inject
     @Claim("appl")
     private ClaimValue<String> custom;
+    
+    @Inject
+    JsonWebToken jwt;
 
     @GET
     @RolesAllowed("protected")
-    public String getJWTBasedValue() {
-        System.out.println(custom.getValue());
-        return "protected";
+    public Account getJWTBasedValue() {
+        Account account = new Account();
+        account.setEmail(jwt.getClaim("sub").toString());
+        return account;
     }
 }
